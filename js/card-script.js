177f805e576de317b99c9fcf84fb49f79be13f03
willrod42 <william@rodriguezfamily.tv>
2019-11-04 18:21:21 -0800

    Script to add topic card

--- js/card-script.js
+++ js/card-script.js
@@ -5,15 +5,7 @@ var makeCard = function (imgPath, topicName) {
     cardTitle = this.cardDiv.append('<div class="card-title"></div>');
     cardTitle.html('<h3>' + topicName + '</h3>');
 
+    cardDiv.css('background-image', 'url(' + imgPath + ')');
+
     return cardDiv;
 }
-
-/*console.log(makeCard('../img/test.png', 'test'));
-
-let card = makeCard('../img/test.png', 'test');
-$('section').append('<div class="flex-item">');*/
-
-$('#test').html('TEST');
-
-document.getElementById('test').innerHTML = 'TEST';
-console.log(document.getElementById('test'));
