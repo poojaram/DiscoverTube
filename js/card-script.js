f72ac71906cf9e774e40dd5670f8b7ebf6b3b0ab
willrod42 <william@rodriguezfamily.tv>
2019-11-04 20:27:30 -0800

    Add function to create topic category and populate with cards

--- js/card-script.js
+++ js/card-script.js
@@ -1,11 +1,45 @@
 'use-strict';
 
-var makeCard = function (imgPath, topicName) {
+var makeCard = function (card) {
     cardDiv = $('<div class="flex-item"></div>');
-    cardTitle = this.cardDiv.append('<div class="card-title"></div>');
-    cardTitle.html('<h3>' + topicName + '</h3>');
+    cardDiv.css('background-image', 'url(' + card.img + ')');
 
-    cardDiv.css('background-image', 'url(' + imgPath + ')');
+    cardTitle = $('<div class="card-title"></div>');
+    cardTitle.html('<h3>' + card.name + '</h3>');
 
+    cardDiv.append(cardTitle);
     return cardDiv;
 }
+
+var cardObjs = [{'name':'Film &amp; Animation', 'img':'../img/film2.jpg'}, 
+                {'name':'Music', 'img':'../img/music.jpg'}, 
+                {'name':'Pets &amp; Animals', 'img':'../img/pets_2.png'}, 
+                {'name':'Sports', 'img':'../img/sports.jpg'}, 
+                {'name':'Gaming', 'img':'../img/gaming.jpg'}, 
+                {'name':'People &amp; Blogs', 'img':'../img/people.jpg'}, 
+                {'name':'Comedy', 'img':'../img/comedy.jpg'}, 
+                {'name':'Entertainment', 'img':'../img/entertainment.jpg'}, 
+                {'name':'Travel &amp; Events', 'img':'../img/travel.jpg'}, 
+                {'name':'Cars', 'img':'../img/autos.jpg'}, 
+                {'name':'News &amp; Politics', 'img':'../img/news.jpg'}, 
+                {'name':'How-to', 'img':'../img/howto.jpg'}, 
+                {'name':'Education', 'img':'../img/education.jpg'}, 
+                {'name':'Science &amp; Technology', 'img':'../img/science.jpg'}, 
+                {'name':'Nonprofits &amp; Activism', 'img':'../img/activism.jpg'}];
+
+//Assumes same number of card names and img urls and that the indices correlate,
+// e.g. the first card name goes with the first card image url, etc.
+var makeTopicSection = function(topicName, cards) {
+    var topicSection = $('<section>');
+    topicSection.append('<h2>' + topicName + '</h2>');
+
+    var container = $('<div class="flex-container">');
+    for(let i = 0; i < cards.length; i++) {
+        container.append(makeCard(cards[i])[0]);
+    }
+
+    topicSection.append(container);
+    $('#watch').append(topicSection);
+}
+
+makeTopicSection('test', cardObjs);
